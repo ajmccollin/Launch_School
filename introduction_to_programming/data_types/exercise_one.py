dd9b8f85a7b82aa5f28c4a08f17c0daee2c088cf
John McCollin <jmccollinjrswe@gmail.com>
2024-08-11 09:40:26 -0600

    Add data type exercises

---
+++ introduction_to_programming/data_types/exercise_one.py
@@ -0,0 +1,29 @@
+print('1. ' + "'True'" + ' is a string')
+print(type('True'))
+
+print('2. ' + 'False' + ' is a Boolean')
+print(type(False))
+
+print('3 ' + '(1, 2, 3)' + ' is a tuple')
+print(type((1, 2, 3)))
+
+print('4. ' + '1.5' + ' is a float')
+print(type(1.5))
+
+print('5. ' + '[1, 2, 3]' + ' is a list')
+print(type([1, 2, 3]))
+
+print('6. ' + '2' + ' is an integer')
+print(type(2))
+
+print('7. ' + 'range(5)' + ' is a range')
+print(type(range(5)))
+
+print('8. ' + '{1, 2, 3}' + ' is a set')
+print(type({1, 2, 3}))
+
+print('9. ' + 'None' + ' has type NoneType')
+print(type(None))
+
+print('10. ' + "{'foo': 'bar'}" + ' is a dictionary')
+print(type({'foo': 'bar'}))
---
+++ introduction_to_programming/data_types/exercise_three.py
@@ -0,0 +1,17 @@
+pets = {
+    'Gordon': 'Fish',
+    'Duck': 'Duck',
+    'Crow': 'Duck',
+    'Dexter': 'Dog', 
+    'Princess': 'Dog',
+    'Vincent': 'Dog',
+    'Oliver': 'Cat',
+    'Brad': 'Lizard',
+    'Blackie': 'Cat',
+    'Callista Flockhart Ally McBeal': 'Cat',
+}
+
+print(type(pets))
+print(pets)
+print(pets['Oliver'])
+print(pets['Vincent'])
