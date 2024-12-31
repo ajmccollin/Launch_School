03dad04002d9503b41f8546ede76d662ac296905
John McCollin <jmccollinjrswe@gmail.com>
2024-12-31 10:46:01 -0700

    Add practice problem

--- py101/small_problems/easy3/ddaaiillyy_ddoouubbllee.py
+++ py101/small_problems/easy3/ddaaiillyy_ddoouubbllee.py
@@ -1,15 +1,21 @@
 def crunch(user_string):
-        new_string = ''
+    if len(user_string) > 0:
+        new_string = user_string[0]
+        index = 0
+    else:
+        return user_string
 
-        for idx, char in enumerate(user_string):
-            if char != user_string[idx - 1]:
-                new_string += char
-        return new_string
+    for char in user_string:
+        if char == new_string[index]:
+            continue
+        new_string += char
+        index += 1
+    return new_string
+
 # These examples should all print True
 print(crunch('ddaaiillyy ddoouubbllee') == 'daily double')
 print(crunch('4444abcabccba') == '4abcabcba')
 print(crunch('ggggggggggggggg') == 'g')
-print(crunch('ggggggggggggggg'))
 print(crunch('abc') == 'abc')
 print(crunch('a') == 'a')
 print(crunch('') == '')
