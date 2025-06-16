d8964259a10e9eee20bec43cc8d520be2fceb12d
John McCollin <jmccollinjrswe@gmail.com>
2025-06-15 20:37:42 -0600

    Add practice problem with solution

---
+++ py110/small_problems/easy2/multiplicative_average.py
@@ -0,0 +1,13 @@
+def multiplicative_average(lst):
+    total_product = 1
+    for num in lst:
+        total_product *= num
+    average = total_product / len(lst)
+    return f"{average:.3f}"
+
+# All of these examples should print True
+print(multiplicative_average([3, 5]) == "7.500")
+print(multiplicative_average([2, 5, 8]) == "26.667")
+print(multiplicative_average([2, 5]) == "5.000")
+print(multiplicative_average([1, 1, 1, 1]) == "0.250")
+print(multiplicative_average([2, 5, 7, 11, 13, 17]) == "28361.667")
